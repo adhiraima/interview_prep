35663fd82dd1c274920ada846e8bb095a754a825
Adhir Aima <adhiraima@Adhirs-MacBook-Pro.local>
2024-07-02 14:21:10 +0530

    Day 2

---
+++ lists/linked_list.py
@@ -0,0 +1,11 @@
+from node import Node
+
+def remove_duplicates(node: Node):
+    pass
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
--- strings/strings1.py
+++ strings/strings1.py
@@ -14,10 +14,50 @@ def unique_chars(s: str) -> bool:
     return True
 
 
+def remove_dupes_bf(s: str) -> str:
+    for i in range(0, len(s) - 1):
+        j = i + 1
+        while j < len(s):
+            if s[i] == s[j]:
+                s =  s[:j] + s[j + 1:]
+            else:
+                j += 1
+    return s
+
+def anagrams(s: str, x: str) -> bool:
+    inter_x = [0] * 256
+    inter_s = [0] * 256
+    if len(s) != len(x):
+        return False
+    else:
+        for i in range(0, len(x)):
+            inter_x[ord(x[i])] += 1
+            inter_s[ord(s[i])] += 1
+    for i in range(0, 256):
+        if inter_x[i] == 0 and inter_s[i] == 0:
+            continue
+        print(f"Comparing {chr(i)}: {inter_x[i]} with {inter_s[i]}")
+        if inter_x[i] != inter_s[i]:
+            return False
+    # print(inter)
+    return True
+
+def encode_spaces(s: str) ->  str:
+    return ''.join([x if x != " " else "%20" for x in s])
+
 def main():
-    print(f"Unique chars in abcdef: {unique_chars('abcdef')}")
-    print(f"Unique chars in abcaef: {unique_chars('abcaef')}")
-    print(f"Unique chars in aabcdeef: {unique_chars('aabcdeef')}")
+    # print(f"Unique chars in abcdef: {unique_chars('abcdef')}")
+    # print(f"Unique chars in abcaef: {unique_chars('abcaef')}")
+    # print(f"Unique chars in aabcdeef: {unique_chars('aabcdeef')}")
+    # print(f"Response: {remove_dupes_bf("abcdaabcd")}")
+    # print(f"Response: {remove_dupes_bf("aaaa")}")
+    # print(f"Response: {remove_dupes_bf("ab")}")
+    # print(f"Response: {remove_dupes_bf("a")}")
+    # print(f"Anagrams: abcd & bcda {anagrams('abcd', 'bcda')}")
+    # print(f"Anagrams: abbcddddcca & ccbcdaabddd {anagrams('abbcddddcca', 'ccbcdaabddd')}")
+    # print(f"Anagrams: abc & bcda {anagrams('abc', 'bcda')}")
+    #print(f"Anagrams: abcddd & bbbcda {anagrams('abcddd', 'bbbcda')}")
+    print(f"""String 'This is a beautiful day' after encoding: {encode_spaces('This is a beautiful day')}""")
 
 if __name__ == "__main__":
     main()
