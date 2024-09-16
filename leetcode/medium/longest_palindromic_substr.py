cd66ea9c2e8f017adade6e8c50284db10c415764
Adhir Aima <adhir.aima@gmail.com>
2024-09-16 09:59:51 +0530

    longest palindromic substring - medium

---
+++ leetcode/medium/longest_palindromic_substr.py
@@ -0,0 +1,27 @@
+def longestPalindrome(self, s):
+    if len(s) <= 1:
+        return s
+
+    def expand_string(left, right):
+        while left >= 0 and right < len(s):
+            if s[left] == s[right]:
+                left -= 1
+                right += 1
+            else:
+                break
+        return s[left + 1:right]
+
+    max = 0
+    result = ""
+    for i in range(len(s)):
+        even = expand_string(i, i)
+        odd = expand_string(i, i + 1)
+
+        if len(even) > max:
+            max = len(even)
+            result = even
+
+        if len(odd) > max:
+            max = len(odd)
+            result = odd
+    return result 
